4ff6ac139c388d1d1a588f54193834908049428c
duperez <eduardobianconiperez@gmail.com>
2024-04-03 19:15:23 -0300

    first commit

--- src/main/java/duperez/moresigns/MoreSigns.java
+++ src/main/java/duperez/moresigns/MoreSigns.java
@@ -1,19 +1,16 @@
 package duperez.moresigns;
 
 import duperez.moresigns.client.ClientSetup;
+import duperez.moresigns.common.CommonSetup;
 import duperez.moresigns.common.registry.*;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import duperez.moresigns.common.CommonSetup;
-import duperez.moresigns.common.Configuration;
 
 import javax.annotation.Nonnull;
 
@@ -36,8 +33,8 @@ public class MoreSigns {
         modEventBus.addListener(CommonSetup::init);
         modEventBus.addListener(ClientSetup::init);
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Configuration.COMMON_CONFIG);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Configuration.CLIENT_CONFIG);
+        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Configuration.COMMON_CONFIG);
+        //ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Configuration.CLIENT_CONFIG);
 
         ModItems.ITEMS.register(modEventBus);
         ModBlocks.BLOCKS.register(modEventBus);
--- src/main/java/duperez/moresigns/common/block/state/CanvasSign.java
+++ src/main/java/duperez/moresigns/common/block/state/CanvasSign.java
@@ -1,23 +1,21 @@
 package duperez.moresigns.common.block.state;
 
-import net.minecraft.world.item.DyeColor;
-import duperez.moresigns.common.Configuration;
+import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nullable;
 
-public interface CanvasSign
-{
-	/**
-	 * Returns this sign's background dye color. If null, the sign is uncolored (beige).
-	 */
-	@Nullable
-	String getBackgroundColor();
+public interface CanvasSign {
+    /**
+     * Returns this sign's background dye color. If null, the sign is uncolored (beige).
+     */
+    @Nullable
+    String getBackgroundColor();
 
-	/**
-	 * Checks a config to determine if the background color is considered "dark".
-	 */
-	default boolean isDarkBackground() {
-		String backgroundDye = this.getBackgroundColor();
-		return backgroundDye != null && Configuration.CANVAS_SIGN_DARK_BACKGROUND_LIST.get().contains(backgroundDye);
-	}
+    /**
+     * Checks a config to determine if the background color is considered "dark".
+     */
+    default boolean isDarkBackground() {
+        String backgroundDye = this.getBackgroundColor();
+        return backgroundDye != null && ImmutableList.of("gray", "purple", "blue", "brown", "green", "red", "black").contains(backgroundDye);
+    }
 }
