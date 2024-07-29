5b8b095944544b73e0bc1c39171a9f5059688dde
LUSA <107564399+LooosaZ@users.noreply.github.com>
2024-07-29 22:14:01 +0100

    changed blue and lime text color + texas texture update

--- src/main/java/duperez/moresigns/common/block/state/CanvasSign.java
+++ src/main/java/duperez/moresigns/common/block/state/CanvasSign.java
@@ -16,6 +16,6 @@ public interface CanvasSign {
      */
     default boolean isDarkBackground() {
         String backgroundDye = this.getBackgroundColor();
-        return backgroundDye != null && ImmutableList.of("gray", "purple", "blue", "brown", "green", "red", "black", "white", "indigo", "loosaz").contains(backgroundDye);
+        return backgroundDye != null && ImmutableList.of("gray", "purple", "brown", "green", "red", "black", "white", "indigo", "loosaz", "lime").contains(backgroundDye);
     }
 }
