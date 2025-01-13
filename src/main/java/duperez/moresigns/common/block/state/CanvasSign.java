846d058d8e048389f922680d4bd3a3866f67856a
LUSA <107564399+LooosaZ@users.noreply.github.com>
2025-01-13 23:34:31 +0000

    1.3.2

--- src/main/java/duperez/moresigns/common/block/state/CanvasSign.java
+++ src/main/java/duperez/moresigns/common/block/state/CanvasSign.java
@@ -16,6 +16,6 @@ public interface CanvasSign {
      */
     default boolean isDarkBackground() {
         String backgroundDye = this.getBackgroundColor();
-        return backgroundDye != null && ImmutableList.of("gray", "purple", "brown", "green", "red", "black", "white", "indigo", "loosaz", "lime", "jinx", "spades", "rabbit", "gold", "mary").contains(backgroundDye);
+        return backgroundDye != null && ImmutableList.of("gray", "purple", "brown", "green", "red", "black", "white", "indigo", "loosaz", "lime", "jinx", "spades", "rabbit", "gold", "mary", "cyber").contains(backgroundDye);
     }
 }
